cad315cc6bfe24d3948995ac2c41a9b543407778
Hamza <hamza38954017@gmail.com>
2026-05-22 03:43:39 +0000

    Revert "Initial commit: Uploaded project files from Colab"
    This reverts commit 35bbe7d87194b0c645609c5a86cde806377596b7.

--- ghorapay/api/get_settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../config.php';
-setCorsHeaders();
-$settings = getSettings();
-jsonResponse(['success'=>true,'min_amount'=>$settings['min_amount'],'max_amount'=>$settings['max_amount']]);
