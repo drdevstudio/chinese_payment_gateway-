dbb8ff6d841a46f1617a64351f5d34211fd31ea0
hamza38954017 <hamza38954017@gmail.com>
2026-05-22 12:19:48 +0530

    Update create-link.php

--- merchant/create-link.php
+++ merchant/create-link.php
@@ -2,7 +2,8 @@
 require_once __DIR__.'/../config.php';
 require_once __DIR__.'/../includes/auth.php';
 require_once __DIR__.'/../includes/merchant_layout.php';
-$merchant = merchantAuth();
+$m = merchantAuth();
+$merchant = fbGet('merchants/' . $m['merchant_id']);
 $settings = getSettings();
 merchantLayoutHead('Create Payment Link');
 ?>
